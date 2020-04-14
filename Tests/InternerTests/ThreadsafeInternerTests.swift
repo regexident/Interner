4206875fa20a87627b40fa6ccddad54f85b6c4eb
Vincent Esche <regexident@gmail.com>
2020-04-14 12:21:15 +0200

    Improved `ThreadsafeInternerTests`

--- Tests/InternerTests/ThreadsafeInternerTests.swift
+++ Tests/InternerTests/ThreadsafeInternerTests.swift
@@ -9,15 +9,15 @@ final class ThreadsafeInternerTests: XCTestCase {
         let interner = ObjectInterner(interner: .init(cachingLookup: true))
 
         let numberOfObjects: Int = 100
-        let duplicatesPerObject: Int = 100
-
         let objects: [Object] = (0..<numberOfObjects).map { _ in
             Object.random(in: Object.min..<Object.max)
         }
 
-        DispatchQueue.concurrentPerform(iterations: objects.count * duplicatesPerObject) { index in
-            let object = objects[index % objects.count]
-            let _ = interner.interned(object)
+        let numberOfThreads: Int = 100
+        DispatchQueue.concurrentPerform(iterations: numberOfThreads) { index in
+            for object in objects {
+                interner.intern(object)
+            }
         }
 
         XCTAssertEqual(interner.count, objects.count)
